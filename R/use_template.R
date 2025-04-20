773ea93e946eeb793d31091bf52da301de1ff3f4
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-04-20 17:27:31 +0200

    Add template check

--- R/use_template.R
+++ R/use_template.R
@@ -14,8 +14,14 @@
 use_template <- function(template, name = template, package = "ktools", path = getwd()){
 
   cat("- Copy", name, "from template: ")
-  template <- system.file(name, package = package)
-  file.copy(from = template, to = file.path(path, name))
-  cat("done \n")
+
+  # -- get template url
+  template <- system.file(template, package = package)
+
+  # -- check & copy
+  if(template != ""){
+    file.copy(from = template, to = file.path(path, name))
+    cat("done \n")
+  } else cat("template not found! \n")
 
 }
