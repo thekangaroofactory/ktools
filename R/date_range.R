20f60d546067f080138b2218e256559fcc6240eb
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-11-07 11:05:58 +0100

    Fixed #28

--- R/date_range.R
+++ R/date_range.R
@@ -23,15 +23,21 @@ date_range <- function(min, max, type = "this_year"){
     floor_year <- lubridate::floor_date(Sys.Date(), unit = "year")
     ceiling_year <- lubridate::ceiling_date(Sys.Date(), unit = "year") - 1
 
+    # -- slice input range
     if(min < floor_year)
       min <- floor_year
 
     if(max > ceiling_year)
       max <- ceiling_year
 
+    # -- when max < this year
+    # pick start of max's year
+    if(min > max)
+      min <- lubridate::floor_date(max, unit = "year")
+
   }
 
   # -- return
-  range <- c(min, max)
+  c(min, max)
 
 }
