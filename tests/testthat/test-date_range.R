2e600cfe290e11c4f3eb5b59cf39ddc5b6efc091
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-01-28 11:01:12 +0100

    Test coverage #31

--- tests/testthat/test-date_range.R
+++ tests/testthat/test-date_range.R
@@ -58,3 +58,17 @@ test_that("date_range [this_year] works", {
   expect_equal(x, c(as.Date(paste0(year, "-01-11")), as.Date(paste0(year, "-12-22"))))
 
 })
+
+
+test_that("date_range ]past_year] works", {
+
+  min <- Sys.Date() - 365 * 2
+  max <- Sys.Date() - 365
+
+  # -- function call
+  x <- date_range(min = min, max = max, type = "this_year")
+
+  # -- check
+  expect_equal(x, c(lubridate::floor_date(Sys.Date() - 365, unit = "year"), Sys.Date() - 365))
+
+})
