a2f7d7109cffef4976fb8b34359e771ea85a7e14
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-04-12 14:25:18 +0200

    Fixed #50

--- R/with_js.R
+++ R/with_js.R
@@ -29,7 +29,7 @@ with_js <- function(package, src, script ,session = shiny::getDefaultReactiveDom
   # -- compute object name in userData (as package_script)
   name <- paste(package, tools::file_path_sans_ext(basename(script)), collapse = "_")
 
-  if(is.null(session$userData$name)){
+  if(is.null(session$userData[[name]])){
 
     # -- insert JS code on client side
     shiny::insertUI(selector = "head",
@@ -40,7 +40,7 @@ with_js <- function(package, src, script ,session = shiny::getDefaultReactiveDom
                                                    script = script))
 
     # -- log it in memory
-    session$userData$name <- TRUE
+    session$userData[[name]] <- TRUE
 
   }
 
