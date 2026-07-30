e119e31f9b8ebe56444142976dbda92faf70e045
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-30 19:15:09 +0200

    Fixed #70

--- R/copy_template.R
+++ R/copy_template.R
@@ -4,14 +4,15 @@
 #'
 #' @param template the name of the template file to be copied
 #' @param path destination path where to copy the template
+#' @param rename the name of the copy
 #'
 #' @returns the output of the file.copy function call
 #' @export
 #'
 #' @examples
-#' copy_template(template = "template_shiny_server", path = "shinyapp")
+#' copy_template(template = "template_shiny_server", path = "shinyapp", rename = TRUE)
 
-copy_template <- function(template, path = getwd()){
+copy_template <- function(template, path = getwd(), filename = NULL){
 
   cat("Copy template:", template, "to destination path =", path, "\n")
 
@@ -22,7 +23,10 @@ copy_template <- function(template, path = getwd()){
     cat("[Warning] template file is not found!")
     return(FALSE)}
 
+  if(is.null(rename))
+    filename <- gsub("template_", "", template)
+
   # -- copy to destination path (+ return output value)
-  file.copy(from = target, to = path)
+  file.copy(from = target, to = filet.path(path, filename))
 
 }
