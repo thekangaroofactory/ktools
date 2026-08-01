a9e599ec214e371c6fe83195256caa89857079b4
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-01 12:31:54 +0200

    Fixed #71

--- R/copy_template.R
+++ R/copy_template.R
@@ -3,6 +3,7 @@
 #' Copy Package Template Files
 #'
 #' @param template the name of the template file to be copied
+#' @param package the name of the package where to find the template (default = ktools)
 #' @param path destination path where to copy the template
 #' @param filename the name of the copy
 #'
@@ -12,20 +13,20 @@
 #' @examples
 #' copy_template(template = "template_shiny_server", path = "shinyapp", filename = "my_server.R")
 
-copy_template <- function(template, path = getwd(), filename = NULL){
+copy_template <- function(template, pkg = "ktools", path = getwd(), filename = NULL){
 
   cat("Copy template to destination path =", path, "\n")
 
   # -- get template file
   target <- system.file(template, package = "ktools")
-
   if(target == "")
     stop("Template file is not found in the package!", call. = F)
 
+  # -- remove "template_"
   if(is.null(filename))
     filename <- gsub("template_", "", template)
 
-  # -- copy to destination path (+ return output value)
+  # -- copy to destination path & return
   file.copy(from = target, to = file.path(path, filename))
 
 }
