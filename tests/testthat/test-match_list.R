f391a0c8f675bfe9f1694cdba6e1c034a872a76f
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-02 12:17:06 +0200

    match.list #73

--- tests/testthat/test-match_list.R
+++ tests/testthat/test-match_list.R
@@ -16,4 +16,9 @@ test_that("with_js works", {
   expect_error(match.list(x = list(foo = 1),
                           reference = list(foo = "1")))
 
+  # -- dummy use case
+  # x is NULL
+  expect_identical(match.list(x = NULL, reference = list(foo = 1)),
+                   list(foo = 1))
+
 })
