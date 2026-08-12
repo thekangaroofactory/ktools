7085b1b481b93cf706cb938371229b2c57101551
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-12 17:24:02 +0200

    Fix test path #73

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,7 +3,8 @@
 create_folder <- function(){
 
   # -- create folder
-  testdata_path <- file.path(system.file("tests", "testthat", package = "ktools"), "testdata")
+  #testdata_path <- file.path(system.file("tests", "testthat", package = "ktools"), "testdata")
+  testdata_path <- file.path(test_path(), "testdata")
   dir.create(testdata_path, recursive = TRUE, showWarnings = FALSE)
 
   # -- return
