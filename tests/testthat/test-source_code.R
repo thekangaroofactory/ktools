8bd4ae687145121f3a4dc79c028bd53378f6f368
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-13 09:52:30 +0200

    Clean env #73

--- tests/testthat/test-source_code.R
+++ tests/testthat/test-source_code.R
@@ -12,4 +12,7 @@ test_that("source_code works", {
   env_after <- utils::lsf.str(envir = globalenv())
   expect_true(length(env_after) >= length(env_before))
 
+  # -- cleanup environment
+  rm(list = ls())
+
 })
