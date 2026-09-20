39fe2d25d45b60f2812db05e09dfc33735069038
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-20 10:41:14 +0200

    Fixed #89 #90 #93

--- R/onclick_event.R
+++ R/onclick_event.R
@@ -3,50 +3,60 @@
 #' Build onClick Event
 #'
 #' @description
-#' This function helps building onclick instruction for a listener
-#' to apply / decode.
-#' This is particularly usefull when working with lists of links, most probably
+#' This function helps building onclick event to pass complex instructions to
+#' a given listener.
+#' This is particularly useful when working with lists of links, most probably
 #' generated within an apply instruction over a data.frame.
 #'
-#' @param target the input to update on click
-#' @param value the value to send to the input
-#' @param namespace the id of the module to target if so
+#' @param target the input to update on click.
+#' @param value the value to send to the input (see details).
+#' @param namespace the id of the module to target if so.
+#' @param event should Shiny treat it as an event.
 #'
 #' @details
 #' `target` defines the name of the input entry to update (i.e. input$target).
 #'
-#' When `value` is NULL (default), the id of the html tag on which the onClick
-#' is applied will be sent.
-#' This enables both strategies:
-#' - send the id of the tag that has been clicked
-#' - send an instruction for the target to decode (see examples)
+#' `value` will be passed to the `target` input.
+#' By default, it will send the id of the HTML tag (ex: actionLink, actionButton).
 #'
-#' @returns a string to pass to an onclick parameter
+#' This behavior can be tuned to send a more complex instruction.
+#' In particular, a list can be passed to the target input (see example).
+#'
+#' @seealso [action_link()]
+#'
+#' @returns a character string.
 #' @export
+#' @importFrom jsonlite toJSON
 #'
 #' @examples
-#' # -- basic use case:
+#' \dontrun{
+#' # baseline:
 #' # input$btn_clicked will get the id of the html tag
 #' onclick_event(target = "btn_clicked")
 #'
-#' # -- custom value:
-#' # input$btn_clicked will get "foo" each time the tag is clicked
+#' # custom value:
+#' # input$btn_clicked will get "foo"
 #' onclick_event(target = "btn_clicked", value = "foo")
 #'
+#' # custom list:
+#' # input$btn_clicked will get the list
+#' onclick_event(target = "btn_clicked", value = list(a = "foo", b = "bar"))
+#'
 #' # -- shiny module:
-#' # input$btn_clicked inside "module" will get 12 each time the tag is clicked
-#' onclick_event(target = "btn_clicked", value = 12, namespace = "module")
+#' # input$btn_clicked inside "module_id" will get 12
+#' onclick_event(target = "btn_clicked", value = 12, namespace = "module_id")
+#' }
 
-onclick_event <- function(target, value = NULL, namespace = NULL){
+onclick_event <- function(target, value = "this.id", namespace = NULL, event = TRUE){
 
-  # -- namespace (ok when NULL)
+  # -- namespace (still works when NULL)
   ns <- shiny::NS(namespace)
 
-  # -- check value
-  if(is.null(value))
-    value <- "this.id"
+  # -- when value is list, it needs to be turned into a json
+  if(is.list(value))
+    value <- jsonlite::toJSON(value, auto_unbox = T)
 
   # -- return
-  paste0('Shiny.setInputValue(\"', ns(target), '\", ', value, ', {priority: \"event\"})')
+  paste0('Shiny.setInputValue("', ns(target), '", ', value, if(event) ', {priority: "event"}', ')')
 
 }
